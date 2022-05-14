5a99960f3ffa7c7ec8c4497bdade989147fcdb5a
matti16 <bmattia92@gmail.com>
2022-05-14 14:11:10 +0200

    upoad img to s3

--- web-app/src/app.py
+++ web-app/src/app.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from models import Trip
 import boto3
 import json
+import os
 
 from config import BASE_PATH, DYNAMO_DB_TABLE, S3_BUCKET
 
@@ -43,11 +44,14 @@ async def post_trip_img(
         trip_id: str = Form(...)
     ):
     s3_path = f"trips_images/{vehicle_id}/{trip_id}/{img.filename}"
-    with open(f"/tmp/{s3_path}", "wb+") as tmp_file:
+    tmp_dir = f"/tmp/trips_images/{vehicle_id}/{trip_id}"
+    os.makedirs(tmp_dir, exist_ok=True)
+    
+    with open(f"{tmp_dir}/{img.filename}", "wb+") as tmp_file:
         tmp_file.write(img.read())
 
     client = boto3.client('s3')
-    with open(f"/tmp/{s3_path}", "rb") as tmp_file:
+    with open(f"{tmp_dir}/{img.filename}", "rb") as tmp_file:
         client.upload_fileobj(tmp_file, S3_BUCKET, s3_path)
 
     return {"bucket": S3_BUCKET, "key": s3_path}
