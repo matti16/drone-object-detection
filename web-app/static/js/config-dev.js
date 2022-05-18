c671af14f0c20dfdc233dc6fcb65d9c9b9344a2e
matti16 <bmattia92@gmail.com>
2022-05-18 15:33:50 +0200

    deploy website

---
+++ web-app/static/js/config-dev.js
@@ -0,0 +1,4 @@
+const config = {
+    apiHost: "https://xhpneti85g.execute-api.eu-west-1.amazonaws.com/dev/api",
+    apiToken: "nZrABsS1Q01AL6jF1hSxo7z1tFAHkZDwa1kpPyHG"
+};
--- web-app/static/js/trip.js
+++ web-app/static/js/trip.js
@@ -4,7 +4,28 @@ var app = new Vue({
         trip: null,
         vehicleId: null,
         tripId: null,
-        imgs: []
+        start_time_str: "",
+        end_time_str: "",
+        imgs: [],
+        v_colors: {
+            "vgx": "blue",
+            "vgy": "green",
+            "vgz": "red",
+        },
+        a_colors: {
+            "agx": "blue",
+            "agy": "green",
+            "agz": "red",
+        },
+        r_colors: {
+            "yaw": "blue",
+            "pitch": "green",
+            "roll": "red"
+        },
+        bat_h_colors: {
+            "bat": "red",
+            "h": "blue"
+        }
     },
 
     mounted: async function() {
@@ -19,8 +40,55 @@ var app = new Vue({
         let response = await fetch(url, requestOptions);
         response = await response.json();
         console.log('Response', response);
-        this.trip = response;
+        this.trip = response
+        this.start_time_str = new Date(this.trip["start_time"] + " UTC").toLocaleString();
+        this.end_time_str = new Date(this.trip["end_time"] + " UTC").toLocaleString();
+
+        await Vue.nextTick();
+        this.create_chart('velocities', this.v_colors, 'Velocities');
+        this.create_chart('accelerations', this.a_colors, 'Accelerations');
+        this.create_chart('rotations', this.r_colors, 'Yaw, Pitch and Roll');
+        this.create_chart('bat_and_h', this.bat_h_colors, 'Battery and Height');
     },
 
-    methods: {}
+    methods: {
+        create_chart: function(divId, colorsDict, title) {
+            var xs = unpack(this.trip.steps, "dt");
+            var data = [];
+            for (const [key, value] of Object.entries(colorsDict)) {
+                let trace = {
+                    type: "scatter",
+                    mode: "lines",
+                    name: key,
+                    x: xs,
+                    y: unpack(this.trip.steps, key),
+                    line: {color: value}
+                }
+                data.push(trace);
+            }
+            var layout = {
+                title: title,
+                paper_bgcolor: "#2c2c2c", 
+                plot_bgcolor: "#2c2c2c",
+                font: {
+                    color: "#fafafa",
+                    family: '"Dosis", sans-serif'
+                },
+                yaxis: {
+                    tickcolor: "#fafafa",
+                    gridcolor: "rgba(255,255,255,0.6)",
+                },
+                xaxis: {
+                    tickcolor: "#fafafa",
+                    gridcolor: "rgba(255,255,255,0.6)",
+                }
+            };
+            Plotly.newPlot(divId, data, layout);
+        }
+    }
 });
+
+
+function unpack(rows, key) {
+    return rows.map(function(row) { return row[key]; });
+}
